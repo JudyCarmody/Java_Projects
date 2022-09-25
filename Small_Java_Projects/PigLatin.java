3308d9296ed4fd49b415bf2cef38d3f89b8f0b87
Judith Oller <judy.e.carmody@gmail.com>
2022-09-25 07:31:25 -0500

    Update PigLatin.java
    Works for words beginning with consonants and vowels.

--- Small_Java_Projects/PigLatin.java
+++ Small_Java_Projects/PigLatin.java
@@ -7,26 +7,29 @@ public class PigLatin {
     public static void main(String[] args){
         Scanner input = new Scanner(System.in);
         String ending = "ay";
+        String vowelEnd = "yay";
         System.out.println("Enter a word: ");
         String originalStr = input.nextLine();
         char[] chaArr = originalStr.toCharArray();
+        boolean isVowel = false;
 
-        /*
-            if initial character is NOT a vowel,
-            move it to the end of array and add "ay"
-        */
-        if(chaArr[0] != 'a' && chaArr[0] != 'e' && chaArr[0] != 'i' && chaArr[0] != 'o' && chaArr[0] != 'u'){
-            char initial = chaArr[0];    // store initial char if NOT vowel.
-            for(int i = 0; i< chaArr.length-1; i++){
-                chaArr[i] = chaArr[i+1];
-            }
-            //String initialStr = String.valueOf(initial);
-            int arrSize = originalStr.length();
-            chaArr[arrSize-1] = initial;
-            String newStr = String.valueOf(chaArr);
-            System.out.println(newStr);
-            System.out.println(newStr + ending);
+        // set boolean to TRUE if initial character is a vowel
+        if(chaArr[0] == 'a' || chaArr[0] == 'e' || chaArr[0] == 'i' || chaArr[0] == 'o' || chaArr[0] == 'u'){ isVowel = true; }
+
+        // store initial char
+        char initial = chaArr[0];
+        for(int i = 0; i< chaArr.length-1; i++){
+            chaArr[i] = chaArr[i+1];
         }
+
+        // create the Pig Latin string
+        int arrSize = originalStr.length();
+        chaArr[arrSize-1] = initial;
+        String newStr = String.valueOf(chaArr);
+        
+        if(isVowel){ System.out.println(newStr + vowelEnd); }
+        else{ System.out.println(newStr + ending); }
+
         input.close();
     }
 }
